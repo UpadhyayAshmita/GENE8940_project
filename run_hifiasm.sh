f3300cc9cec25e8dad2f2e32f9bb1684d531d905
Ashmita <ashmitau@uark.edu>
2025-03-30 20:01:17 -0400

    added core number

--- run_hifiasm.sh
+++ run_hifiasm.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-#SBATCH --job-name=run_hifiasm	                                # Job name
-#SBATCH --partition=batch		                                # Partition (queue) name
-#SBATCH --ntasks=1			                                    # Single task job
-#SBATCH --cpus-per-task=6		                                # Number of cores per task - match this to the num_threads used by BLAST
-#SBATCH --mem=128gb			                                    # Total memory for job
-#SBATCH --time=48:00:00  		                                # Time limit hrs:min:sec
-#SBATCH --output=/work/yclab/au08019/hifiasm.log.%j	    # Location of standard output and error log files (replace cbergman with your myid)
-#SBATCH --mail-user=au08019@uga.edu                            # Where to send mail (replace cbergman with your myid)
-#SBATCH --mail-type=END,FAIL                                    # Mail events (BEGIN, END, FAIL, ALL)
+#SBATCH --job-name=run_hifiasm
+#SBATCH --partition=batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=128gb
+#SBATCH --time=48:00:00
+#SBATCH --output=/work/yclab/au08019/hifiasm.log.%j
+#SBATCH --mail-user=au08019@uga.edu
+#SBATCH --mail-type=END,FAIL
 
-# Set the output directory variable
+# Output directory
 OUTDIR="/work/yclab/au08019/GENE8940_project/hifiasm"
-# Create the directory if it does not exist
 mkdir -p $OUTDIR
-# Load module or conda env
-module load hifiasm/0.19.6-GCCcore-11.3.0
-# Input and output
-READS="Suziblue_allruns.fastq"
-OUT="SuziBlue_Hifiasm"
 
-# Run hifiasm
-hifiasm -o ${OUT} -t 32 ${READS}
+# Load module
+module load hifiasm/0.19.6-GCCcore-11.3.0
+
+# Input and output
+READS="${OUTDIR}/Suziblue_allruns.fastq"
+OUT="${OUTDIR}/SuziBlue_Hifiasm"
+
+# Run hifiasm in tetraploid mode
+hifiasm -o ${OUT} -t 32 --poly ${READS}
