0dad81653daa34bf77d7b412ae28975ae1984bfe
Ashmita <ashmitau@uark.edu>
2025-04-25 14:07:50 -0400

    added mummerplot filtered code

--- hifiasm_contigs.sh
+++ hifiasm_contigs.sh
@@ -13,27 +13,27 @@
 OUTDIR="/work/yclab/au08019/GENE8940_project/rerun_hifiasm"
 mkdir -p $OUTDIR
 # Load module
-#module load hifiasm/0.24.0-GCCcore-12.3.0 
+module load hifiasm/0.24.0-GCCcore-12.3.0 
 # Input and output
 READS="/home/au08019/blueberry_hifi/Suziblue_allruns.fastq"
 OUT="${OUTDIR}/SuziBlue_Hifiasm"
 
-#Run hifiasm 
+# #Run hifiasm 
 # hifiasm -o ${OUT} -t 32 ${READS}
 
-# first converting the gfa format into fasta format using awk for passing the fasta file to QUAST
+# # first converting the gfa format into fasta format using awk for passing the fasta file to QUAST
 # awk '/^S/{print ">"$2"\n"$3}' $OUTDIR/SuziBlue_Hifiasm.bp.p_ctg.gfa > $OUTDIR/SuziBlue_Hifiasm.bp.p_ctg.fa
-# # awk '/^S/{print ">"$2"\n"$3}' $OUTDIR/SuziBlue_Hifiasm.bp.hap1.p_ctg.gfa > $OUTDIR/SuziBlue_Hifiasm.hap1.fa
-# # awk '/^S/{print ">"$2"\n"$3}' $OUTDIR/SuziBlue_Hifiasm.bp.hap2.p_ctg.gfa > $OUTDIR/SuziBlue_Hifiasm.hap2.fa
+# # # awk '/^S/{print ">"$2"\n"$3}' $OUTDIR/SuziBlue_Hifiasm.bp.hap1.p_ctg.gfa > $OUTDIR/SuziBlue_Hifiasm.hap1.fa
+# # # awk '/^S/{print ">"$2"\n"$3}' $OUTDIR/SuziBlue_Hifiasm.bp.hap2.p_ctg.gfa > $OUTDIR/SuziBlue_Hifiasm.hap2.fa
 
-# # Index the contigs FASTA using samtools
-module load SAMtools/1.16.1-GCC-11.3.0
+# # # Index the contigs FASTA using samtools
+# module load SAMtools/1.16.1-GCC-11.3.0
 # samtools faidx $OUTDIR/SuziBlue_Hifiasm.bp.p_ctg.fa
 # # Get list of contigs >= 50kb and filtering those from the contigs
 # awk '$2 >= 50000' $OUTDIR/SuziBlue_Hifiasm.bp.p_ctg.fa.fai | cut -f1 > $OUTDIR/SuziBlue_Hifiasm.long_contigs.txt
 # # Extract only long contigs into a new FASTA
 # samtools faidx $OUTDIR/SuziBlue_Hifiasm.bp.p_ctg.fa $(cat $OUTDIR/SuziBlue_Hifiasm.long_contigs.txt) > $OUTDIR/SuziBlue_Hifiasm.contigs.50kb.fa
-# assembly evaluation using QUAST
+# #assembly evaluation using QUAST
 # module load QUAST/5.2.0-foss-2022a
 # mkdir -p $OUTDIR/QUAST
 # quast.py \
@@ -44,7 +44,7 @@ module load SAMtools/1.16.1-GCC-11.3.0
 
 # #generating mummerplot
 # mkdir -p $OUTDIR/mummer
-# module load MUMmer/4.0.0rc1-GCCcore-11.3.0
+module load MUMmer/4.0.0rc1-GCCcore-11.3.0
 # #using mummer for hifi assembly
 # nucmer -t 32 /home/au08019/GENE8940_project/DraperChrOrdered_modified.fasta $OUTDIR/SuziBlue_Hifiasm.contigs.50kb.fa -p $OUTDIR/mummer/hifiasmcontigs_vs_ref
 # delta-filter -1 $OUTDIR/mummer/hifiasmcontigs_vs_ref.delta > $OUTDIR/mummer/hifiasmcontigs_vs_ref.1delta
@@ -60,11 +60,9 @@ module load SAMtools/1.16.1-GCC-11.3.0
 #  -c 32 \
 #  --out_path $OUTDIR/BUSCO
 
-
-
 # #running minimap to see how many contigs are overlapping
 # #first creating the reference index
-module load minimap2/2.24-GCCcore-11.3.0
+# module load minimap2/2.24-GCCcore-11.3.0
 # minimap2 -d $OUTDIR/DraperChrOrdered_modified.mmi /home/au08019/GENE8940_project/DraperChrOrdered_modified.fasta
 # minimap2 $OUTDIR/DraperChrOrdered_modified.mmi $OUTDIR/SuziBlue_Hifiasm.contigs.50kb.fa > $OUTDIR/SuziBlue_Hifiasm_vs_Draper.paf
 # head -n 5 $OUTDIR/SuziBlue_Hifiasm_vs_Draper.paf
@@ -72,14 +70,27 @@ module load minimap2/2.24-GCCcore-11.3.0
 # wc -l $OUTDIR/SuziBlue_Hifiasm_vs_Draper.paf
 # cut -f1 $OUTDIR/SuziBlue_Hifiasm_vs_Draper.paf | sort | uniq | wc -l
 
-#generating bam file for visulation further
-minimap2 -ax asm20 $OUTDIR/DraperChrOrdered_modified.mmi $OUTDIR/SuziBlue_Hifiasm.contigs.50kb.fa | \
-samtools view -bS - | \
-samtools sort -@ 32 -o $OUTDIR/SuziBlue_Hifiasm_vs_Draper.bam
+# #generating bam file for visulation further
+# minimap2 -ax asm20 $OUTDIR/DraperChrOrdered_modified.mmi $OUTDIR/SuziBlue_Hifiasm.contigs.50kb.fa | \
+# samtools view -bS - | \
+# samtools sort -@ 32 -o $OUTDIR/SuziBlue_Hifiasm_vs_Draper.bam
 
-#Index the BAM file for IGV
-samtools index $OUTDIR/SuziBlue_Hifiasm_vs_Draper.bam
+# #Indexing the BAM file for IGV
+# samtools index $OUTDIR/SuziBlue_Hifiasm_vs_Draper.bam
 
 # #downloading the pdf and html file 
 # #scp sapelo2:/work/yclab/au08019/GENE8940_project/hifiasm/QUAST/report.pdf .
+
+#further filtering of contigs file of suziblue for mummerplot 
+show-coords -rcl $OUTDIR/mummer/hifiasmcontigs_vs_ref.delta > $OUTDIR/mummer/coords.txt
+awk '$7 >= 10000 && $8 >= 10000 && $9 >= 95' coords.txt > filtered_95percnt_idycord.txt #filtered IDY > 95% only - we have 81,554 in count
+
+# generate filtered delta and cleaner MUMmerplot (IDY ≥ 95%, length ≥ 10kb)
+delta-filter -i 95 -l 10000 $OUTDIR/mummer/hifiasmcontigs_vs_ref.delta > $OUTDIR/mummer/hifiasmcontigs_vs_ref.filtered.delta
+
+# Create filtered plot (PNG and SVG)
+mummerplot --size large --layout --color -f --png $OUTDIR/mummer/hifiasmcontigs_vs_ref.filtered.delta -p $OUTDIR/mummer/hifiasmcontigs_vs_ref.filtered
+mummerplot --size large --layout --color -f --svg $OUTDIR/mummer/hifiasmcontigs_vs_ref.filtered.delta -p $OUTDIR/mummer/hifiasmcontigs_vs_ref.filtered
+
+
 
